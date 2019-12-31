17e166804ea60dad2a78c330fd27774114e03be5
Harsimran Dhoofar <sam.dhoofar@gmail.com>
2019-12-30 22:44:16 -0500

    Done

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { Employee } from 'src/app/shared/employee.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,7 +13,9 @@ import { Employee } from 'src/app/shared/employee.model';
 export class EmployeeListComponent implements OnInit {
 
   list: Employee[];
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService,
+    private firestore:AngularFirestore,
+    private toastr:ToastrService) { }
 
   ngOnInit() {
     this.service.getEmployees().subscribe(actionArray =>{
@@ -24,5 +29,13 @@ export class EmployeeListComponent implements OnInit {
     })
     });
   }
-
+    onEdit(emp:Employee){
+      this.service.formData =Object.assign({},emp );
+    }
+    onDelete(id:string){
+      if(confirm("Are you sure to delete this record?")){
+      this.firestore.doc('employees/'+id).delete();
+      this.toastr.warning('Deleted successfully','EMP, Register')
+      }
+    }
 }
